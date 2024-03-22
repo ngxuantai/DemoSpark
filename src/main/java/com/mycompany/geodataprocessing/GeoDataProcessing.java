655fab57092f897ca72d3015333fdbf51c866dc2
ngxuantai <xuantai1902@gmail.com>
2024-03-22 11:52:13 +0700

    avgScore

--- src/main/java/com/mycompany/geodataprocessing/GeoDataProcessing.java
+++ src/main/java/com/mycompany/geodataprocessing/GeoDataProcessing.java
@@ -9,7 +9,7 @@ public class GeoDataProcessing {
 
         public static void main(String[] args) {
                 SparkSession spark = SparkSession.builder()
-                                .master("spark://172.16.3.203:7077")
+                                .master("spark://laptop-pvpfh1bd:7077")
                                 .appName("GeoDataProcessing")
                                 .getOrCreate();
 
@@ -17,53 +17,33 @@ public class GeoDataProcessing {
                         Dataset<Row> data = spark.read()
                                         .format("csv")
                                         .option("header", "true")
-                                        .load("hdfs://localhost:9000/DataGeo.csv");
+                                        .load("hdfs://localhost:9000/NameStudent.csv");
 
-                        // Tính tổng số địa điểm
-                        double totalPlaces = data.count();
-                        System.out.println("Total places: " + totalPlaces);
+                        Dataset<Row> avgScorePerStudent = data.withColumn("Điểm trung bình",
+                                        (data.col("Toán").cast("double")
+                                                        .plus(data.col("Văn").cast("double"))
+                                                        .plus(data.col("Lý").cast("double"))
+                                                        .plus(data.col("Hóa").cast("double"))
+                                                        .plus(data.col("Sinh").cast("double")))
+                                                        .divide(5));
 
-                        // Tính điểm trung bình của kinh độ và vĩ độ
-                        double avgLongitude = data.agg(org.apache.spark.sql.functions.avg("longitude")).first()
-                                        .getDouble(0);
-                        // chuyển avgLongitude từ kiểu dữ liệu Double về kiểu dữ liệu long
-                        double avgLatitude = data.agg(org.apache.spark.sql.functions.avg("latitude")).first()
-                                        .getDouble(0);
-
-                        System.out.println("Average Longitude: " + avgLongitude);
-                        System.out.println("Average Latitude: " + avgLatitude);
+                        Dataset<Row> avgScorePerAge = avgScorePerStudent.groupBy("Tuổi")
+                                        .avg("Điểm trung bình")
+                                        .withColumnRenamed("avg(Điểm trung bình)", "Điểm trung bình trên theo tuổi");
 
                         // Lưu kết quả vào tệp văn bản
-                        Dataset<Row> results = spark.createDataFrame(
-                                        java.util.Arrays.asList(
-                                                        new org.apache.spark.sql.RowFactory().create("Total Places",
-                                                                        totalPlaces),
-                                                        new org.apache.spark.sql.RowFactory().create(
-                                                                        "Average Longitude",
-                                                                        avgLongitude),
-                                                        new org.apache.spark.sql.RowFactory().create("Average Latitude",
-                                                                        (double) avgLatitude)),
-                                        new org.apache.spark.sql.types.StructType(
-                                                        new org.apache.spark.sql.types.StructField[] {
-                                                                        new org.apache.spark.sql.types.StructField(
-                                                                                        "Metric",
-                                                                                        org.apache.spark.sql.types.DataTypes.StringType,
-                                                                                        false,
-                                                                                        org.apache.spark.sql.types.Metadata
-                                                                                                        .empty()),
-                                                                        new org.apache.spark.sql.types.StructField(
-                                                                                        "Value",
-                                                                                        org.apache.spark.sql.types.DataTypes.DoubleType,
-                                                                                        false,
-                                                                                        org.apache.spark.sql.types.Metadata
-                                                                                                        .empty())
-                                                        }));
-
-                        results.coalesce(1).write()
+                        avgScorePerStudent.coalesce(1)
+                                        .write()
                                         .format("csv")
-                                        .mode(org.apache.spark.sql.SaveMode.Overwrite)
-                                        // .option("header", "true")
-                                        .save("hdfs://localhost:9000/result.csv");
+                                        .mode("overwrite")
+                                        .option("header", "true")
+                                        .save("hdfs://localhost:9000/AverageScorePerStudent.csv");
+
+                        avgScorePerAge.coalesce(1).write()
+                                        .format("csv")
+                                        .mode("overwrite")
+                                        .option("header", "true")
+                                        .save("hdfs://localhost:9000/AverageScorePerAge.csv");
 
                 } catch (Exception e) {
                         e.printStackTrace();
